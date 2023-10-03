9ddf7446fc08b82ce5435c4e63f9f27a4f50da4d
Alif <alifrazanit@gmail.com>
2023-10-03 14:42:46 +0700

    50. Adding normal JavaScript Logic to Components

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,32 +1,22 @@
-// import './ExpenseItem.css';
-// export const ExpenseItem = props =>{
-//     return (
-//         <div className='expense-item'>
-//             <div>{new Date(props.date).toISOString()}</div>
-//             <div className='expense-item__description'>
-//                 <h2>{props.title}</h2>
-//                 <div className='expense-item__price'>Rp. {props.amount}</div>
-//             </div>
-//         </div>
-//     )
-// }
-
-/** ====================== */
-/** = Second Alternative = */
-/** ====================== */
-
-
 import './ExpenseItem.css';
-function ExpenseItem({title, amount, date}) {
+export const ExpenseItem = props =>{
+    const month = new Date(props.date).toLocaleString('en-US', { month: 'long'});
+    const day = new Date(props.date).toLocaleString('en-US', { day: '2-digit'});
+    const year = new Date(props.date).getFullYear(); 
     return (
         <div className='expense-item'>
-            <div>{date}</div>
+            <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
             <div className='expense-item__description'>
-                <h2>{title}</h2>
-                <div className='expense-item__price'>Rp. {amount}</div>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>Rp. {props.amount}</div>
             </div>
         </div>
     )
 }
 
-export default ExpenseItem;
+
+
