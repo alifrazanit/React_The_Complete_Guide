09254402e78d48b92abd8650d4aeb8492026b329
Alif <alifrazanit@gmail.com>
2023-10-03 14:38:46 +0700

    49. Alternative Ways of Passing & Receiving / Handling props

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,12 +1,32 @@
+// import './ExpenseItem.css';
+// export const ExpenseItem = props =>{
+//     return (
+//         <div className='expense-item'>
+//             <div>{new Date(props.date).toISOString()}</div>
+//             <div className='expense-item__description'>
+//                 <h2>{props.title}</h2>
+//                 <div className='expense-item__price'>Rp. {props.amount}</div>
+//             </div>
+//         </div>
+//     )
+// }
+
+/** ====================== */
+/** = Second Alternative = */
+/** ====================== */
+
+
 import './ExpenseItem.css';
-export const ExpenseItem = props =>{
+function ExpenseItem({title, amount, date}) {
     return (
         <div className='expense-item'>
-            <div>{new Date(props.date).toISOString()}</div>
+            <div>{date}</div>
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
-                <div className='expense-item__price'>Rp. {props.amount}</div>
+                <h2>{title}</h2>
+                <div className='expense-item__price'>Rp. {amount}</div>
             </div>
         </div>
     )
 }
+
+export default ExpenseItem;
