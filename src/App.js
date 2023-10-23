767030bb85e0be6a7f3b7ba6319defde47c2d958
Alif Razan Saputra <alifrazanit@gmail.com>
2023-10-23 13:59:08 +0700

    228. Building & Using a Custom Context Provider Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
 
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
@@ -8,37 +8,16 @@ import { useEffect } from "react";
 import { AuthContext } from "./store/auth-context";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    const isLogin = localStorage.getItem("isLogin");
-
-    if (isLogin) {
-      setIsLoggedIn(true);
-    }
-  }, []);
-
-  const loginHandler = (email, password) => {
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    setIsLoggedIn(false);
-  };
+  const ctx = useContext(AuthContext);
 
 
   return (
     <React.Fragment>
-      <AuthContext.Provider value={{
-        isLoggedIn: isLoggedIn,
-        onLogout: logoutHandler
-      }}>
-        <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-        <main>
-          {!isLoggedIn && <Login onLogin={loginHandler} />}
-          {isLoggedIn && <Home onLogout={logoutHandler} />}
-        </main>
-      </AuthContext.Provider>
+      <MainHeader isAuthenticated={ctx.isLoggedIn} onLogout={ctx.onLogout} />
+      <main>
+        {!ctx.isLoggedIn && <Login onLogin={ctx.onLogin} />}
+        {ctx.isLoggedIn && <Home onLogout={ctx.onLogin} />}
+      </main>
     </React.Fragment>
   );
 }
