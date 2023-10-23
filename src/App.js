4638934d86d633cd7d4de0326289bb2ae742edf7
Alif Razan Saputra <alifrazanit@gmail.com>
2023-10-23 23:08:21 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React, { useState } from "react";
 import { Header } from "./components/Header/Header";
-import { Footer } from './components/Footer/Footer';
+import { Footer } from "./components/Footer/Footer";
 
-import { LoginPage } from './pages/Login/LoginPage';
-import { MenuPage } from './pages/Menu/MenuPage';
-import { OrderPage } from './pages/Order/OrderPage';
-
+import { LoginPage } from "./pages/Login/LoginPage";
+import { MenuPage } from "./pages/Menu/MenuPage";
+import { OrderPage } from "./pages/Order/OrderPage";
 
 function App() {
-  const [switchPage, setSwitchPage] = useState('LoginPage');
-  const onSwitchPageHandler = e => {
-    setSwitchPage(e)
-  }
+  const [switchPage, setSwitchPage] = useState("LoginPage");
+  const onSwitchPageHandler = (e) => {
+    setSwitchPage(e);
+  };
   return (
     <>
       <Header />
-      {switchPage && switchPage === 'MenuPage' && (<MenuPage />)}
-      {switchPage && switchPage === 'OrderPage' && (<OrderPage />)}
-      {switchPage && switchPage === 'LoginPage' && (<LoginPage />)}
-      <Footer onSwitchPage={onSwitchPageHandler}/>
+      <div className="container-app">
+        {switchPage && switchPage === "MenuPage" && <MenuPage />}
+        {switchPage && switchPage === "OrderPage" && <OrderPage />}
+        {switchPage && switchPage === "LoginPage" && <LoginPage />}
+      </div>
+      <Footer onSwitchPage={onSwitchPageHandler} />
     </>
   );
 }
--- src/pages/Login/LoginPage.js
+++ src/pages/Login/LoginPage.js
@@ -1,11 +1,31 @@
-
 import "./LoginPage.css";
 import React from "react";
 
 export const LoginPage = (props) => {
-    return (
-        <React.Fragment>
-          <h1>LoginPage</h1>
-        </React.Fragment>
-    )
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    
+  };
+  return (
+    <React.Fragment>
+      <div className="form-container">
+        <form onSubmit={onSubmitForm}>
+          <div className="form-control">
+            <label>Username</label>
+            <input type="text" />
+            <small className="error-not-valid">Username not valid!</small>
+          </div>
+          <div className="form-control">
+            <label>Password</label>
+            <input type="password" />
+          </div>
+          <div className="form-action">
+            <button type="submit" className="btn btn-full btn-success">
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </React.Fragment>
+  );
 };
