aaca54bcc13a7b7b539c2c3e69474e3b14a245e4
Alif Razan Saputra <alifrazanit@gmail.com>
2023-11-09 22:07:24 +0700

    80

--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -1,16 +1,27 @@
+import { useState } from "react"
 const initialGameBoard = [
     [null,null,null],
     [null,null,null],
     [null,null,null]
 ]
 export const GameBoard = props => {
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    const handleSelectedSquer = (rowIndex, colIndex) => {
+        setGameBoard((prevGameBoard) => {
+            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])]
+            updatedBoard[rowIndex][colIndex] = 'X';
+            return updatedBoard;
+        });
+    }
+
     return (
         <ol id="game-board">
-            {initialGameBoard.map((row, rowIndex) => (<li key={rowIndex}>
+            {gameBoard.map((row, rowIndex) => (<li key={rowIndex}>
                 <ol>
                     {row.map((playerSymbol, colIndex) => (
                         <li key={colIndex}>
-                            <button>{playerSymbol}</button>
+                            <button onClick={() => handleSelectedSquer(rowIndex, colIndex)}>{playerSymbol}</button>
                         </li>
                     ))}
                 </ol>
