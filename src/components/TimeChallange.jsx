26cf11d27afa9649a2e0460c603d81c92250f340
Alif Razan Saputra <alifrazanit@gmail.com>
2023-11-26 06:56:08 +0700

    146

--- src/components/TimeChallange.jsx
+++ src/components/TimeChallange.jsx
@@ -1,16 +1,34 @@
-export const TimeChallange = ({ title, targetTime}) => {
-    return (
-        <section className="challenge">
-            <h2>{title}</h2>
-            <p className="challenge-time">
-                {targetTime} second{targetTime > 1 ? 's' : ''}
-            </p>
-            <button>
-                Start Challenge
-            </button>
-            <p>
-                Time is running
-            </p>
-        </section>
-    )
-}
+import { useState } from "react";
+
+export const TimeChallange = ({ title, targetTime }) => {
+  const [timeExpire, setTimeExpire] = useState(false);
+  const [timerStart, setTimerStart] = useState(false);
+
+  const onStart = (e) => {
+    const timer = setTimeout(() => {
+      setTimeExpire(true);
+    }, targetTime * 1000);
+    setTimerStart(true)
+    const clearTimer = () => {
+      clearTimeout(timer);
+    };
+
+    return clearTimer; 
+  };
+
+  return (
+    <section className="challenge">
+      <h2>{title}</h2>
+      {timeExpire && <p>You Lost!</p>}
+      <p className="challenge-time">
+        {targetTime} second{targetTime > 1 ? "s" : ""}
+      </p>
+      <button onClick={onStart}>
+        {timerStart ? "Stop" : "Start"} Challenge
+      </button>
+      <p className={timerStart ? "active" : undefined}>
+        {timerStart ? "Timer is Running..." : "Timer inactive"}
+      </p>
+    </section>
+  );
+};
