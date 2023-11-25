8321742ce92053d90373f48d4f9228cc13c91bb9
Alif Razan Saputra <alifrazanit@gmail.com>
2023-11-25 16:47:16 +0700

    91

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import { Player } from "./components/Player/Player";
 import { GameBoard } from "./components/GameBoard/GameBoard";
 import React, { useState } from "react";
 import { Log } from "./components/Log/Log";
+import { WINNING_COMBINATIONS } from "./winning-combination";
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
 function ActivePlayer(gameTurn) {
   let currentPlayer = "X";
@@ -15,6 +21,30 @@ function ActivePlayer(gameTurn) {
 function App() {
   const [gameTurn, setGameTurn] = useState([]);
   const activePlayer = ActivePlayer(gameTurn);
+  let gameBoard = initialGameBoard;
+  let winner;
+
+  for (const turn of gameTurn) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+
+  for (const combination of WINNING_COMBINATIONS) {
+    console.log('GAMEBOARD', gameBoard)
+    const firstSymbols = gameBoard[combination[0].row][combination[0].column];
+    const secondSymbols = gameBoard[combination[1].row][combination[1].column];
+    const thirdSymbols = gameBoard[combination[2].row][combination[2].column];
+    console.log(firstSymbols, secondSymbols, thirdSymbols)
+    if (
+      firstSymbols &&
+      firstSymbols === secondSymbols &&
+      firstSymbols === thirdSymbols
+    ) {
+      winner = firstSymbols;
+    }
+  }
 
   const setActivePlayerHandler = (rowIndex, colIndex) => {
     setGameTurn((prevGameTurn) => {
@@ -35,7 +65,11 @@ function App() {
           <Player name="Player 1" sysmbol="X" isActive={activePlayer === "X"} />
           <Player name="Player 2" sysmbol="O" isActive={activePlayer === "O"} />
         </ol>
-        <GameBoard onSelectedSquare={setActivePlayerHandler} turns={gameTurn} />
+        {winner && <p>You are the winner, {winner}!</p>}
+        <GameBoard
+          onSelectedSquare={setActivePlayerHandler}
+          board={gameBoard}
+        />
       </div>
       <Log turns={gameTurn} />
     </main>
