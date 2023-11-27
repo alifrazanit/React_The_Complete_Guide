f50bb9d94f736308600c964fceae46b1629c1b38
Alif Razan Saputra <alifrazanit@gmail.com>
2023-11-27 16:48:42 +0700

    169

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
-import Player from './components/Player.jsx';
-import { TimeChallange } from './components/TimeChallange.jsx';
+import { SideBar } from "./components/SideBar";
 
 function App() {
   return (
-    <>
-      <Player />
-      <div id="challenges">
-        <TimeChallange title="Easy" targetTime={1} />
-        <TimeChallange title="Not Easy" targetTime={5} />
-        <TimeChallange title="Getting Though" targetTime={10} />
-        <TimeChallange title="Pros Only" targetTime={15} />
-      </div>
-    </>
+    <main className="h-screen my-8">
+      <SideBar />
+    </main>
   );
 }
 
