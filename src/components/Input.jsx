f7418adee8c741b7d1efc52438cb683d4e491d87
Alif Razan Saputra <alifrazanit@gmail.com>
2023-11-27 16:57:57 +0700

    169

---
+++ src/components/Input.jsx
@@ -0,0 +1,10 @@
+export const Input = ({label, textarea, ...props}) => {
+    return(
+       <>
+        <p>
+            <label>{label}</label>
+            {textarea ? <textarea {...props} /> : <input {...props}/>}
+        </p>
+       </>
+    )
+}
