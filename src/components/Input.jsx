265f7a3c4c0dcd88476609ee145c302bed4a0ef3
Alif Razan Saputra <alifrazanit@gmail.com>
2023-11-28 10:56:37 +0700

    170

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,10 @@
 export const Input = ({label, textarea, ...props}) => {
+    const classsess="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border";
     return(
        <>
-        <p>
-            <label>{label}</label>
-            {textarea ? <textarea {...props} /> : <input {...props}/>}
+        <p className="flex flex-col gap-1 my-4">
+            <label className="text-sm font-bold uppercase text-stone-500">{label}</label>
+            {textarea ? <textarea className={classsess} {...props} /> : <input className={classsess} {...props}/>}
         </p>
        </>
     )
