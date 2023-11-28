5e56165ac19b4d8a7cc3c078f6a19e27c3524067
Alif Razan Saputra <alifrazanit@gmail.com>
2023-11-28 14:43:25 +0700

    173:

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,19 @@
-export const Input = ({label, textarea, ...props}) => {
-    const classsess="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border";
-    return(
-       <>
-        <p className="flex flex-col gap-1 my-4">
-            <label className="text-sm font-bold uppercase text-stone-500">{label}</label>
-            {textarea ? <textarea className={classsess} {...props} /> : <input className={classsess} {...props}/>}
-        </p>
-       </>
-    )
-}
+import react, { forwardRef } from "react";
+export const Input = forwardRef(({ label, textarea, ...props }, ref) => {
+  const classsess =
+    "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border";
+  return (
+    <>
+      <p className="flex flex-col gap-1 my-4">
+        <label className="text-sm font-bold uppercase text-stone-500">
+          {label}
+        </label>
+        {textarea ? (
+          <textarea ref={ref} className={classsess} {...props} />
+        ) : (
+          <input ref={ref} className={classsess} {...props} />
+        )}
+      </p>
+    </>
+  );
+});
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,6 +1,13 @@
-import { Input } from "./Input"
+import { useRef } from "react";
+import { Input } from "./Input";
+
 
 export const NewProject = ({onStartAddProject}) => {
+    const title = useRef();
+    const desc = useRef();
+    const duedate = useRef();
+
+
     return (
         <div className="w-[35rem] mt-16">
             <menu className="flex items-center justify-end gap-4 my-4">
@@ -8,9 +15,9 @@ export const NewProject = ({onStartAddProject}) => {
                 <li><button className="px-6 py-2 text-stone-800 text-stone-50 hover:text-stone-950">Save</button></li>
             </menu>
             <div>
-                <Input label="Title"/>
-                <Input label="Description" textarea/>
-                <Input label="Due Date"/>
+                <Input ref={title} label="Title"/>
+                <Input ref={desc} label="Description" textarea/>
+                <Input ref={duedate} label="Due Date"/>
             </div>
         </div>
     )
